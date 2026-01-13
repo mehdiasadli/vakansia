901497a599ab046725d2b3174c7cadd8634e1490
mehdiasadli <asadlimehdi25@gmail.com>
2026-01-14 03:05:26 +0400

    fix: login schema

--- packages/lib/src/regexes.ts
+++ packages/lib/src/regexes.ts
@@ -2,3 +2,14 @@ export const SLUG_REGEX = /^[a-z0-9-]+$/i;
 export const USERNAME_REGEX = /^[a-z0-9_]+$/i;
 
 export const VOEN_REGEX = /^(\d{2})(\d{6})(\d)(\d)$/;
+
+/**
+ * RULES:
+ *
+ * - At least 1 uppercase letter
+ * - At least 1 lowercase letter
+ * - At least 1 number
+ *
+ * ** No need to check for length, it will be checked by the schema
+ */
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
