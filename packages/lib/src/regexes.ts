1cd7e3e52e8c75ce063c9e5335de692f38109fd0
Mehdi Asadli <mehdiasadli@azericard.com>
2026-01-14 01:53:27 +0400

    fix: remove regex named groups

--- packages/lib/src/regexes.ts
+++ packages/lib/src/regexes.ts
@@ -1,8 +1,7 @@
 export const SLUG_REGEX = /^[a-z0-9-]+$/i;
 export const USERNAME_REGEX = /^[a-z0-9_]+$/i;
 
-export const VOEN_REGEX =
-	/^(?<territory>\d{2})(?<serial>\d{6})(?<checksum>\d)(?<status>\d)$/;
+export const VOEN_REGEX = /^(\d{2})(\d{6})(\d)(\d)$/;
 
 export const PHONE_REGEXES = {
 	// example: 994552001234
