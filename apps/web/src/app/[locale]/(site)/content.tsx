ebaa4887db03408b5c21e7d46496fc6209b7e705
Mehdi Asadli <mehdiasadli@azericard.com>
2026-01-14 01:51:01 +0400

    fix: remove unused components

--- apps/web/src/app/[locale]/(site)/content.tsx
+++ apps/web/src/app/[locale]/(site)/content.tsx
@@ -1,10 +1,5 @@
 "use client";
-import { env } from "@vakansia/env/web";
 
 export default function Content() {
-	console.log("CLIENT:");
-
-	console.log("DOMAINS:", env.NEXT_PUBLIC_DOMAINS.admin);
-
 	return <div>Content</div>;
 }
