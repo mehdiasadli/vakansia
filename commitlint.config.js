a36a1ac08bf6550732b7891ee517b5e18f7963ce
mehdiasadli <asadlimehdi25@gmail.com>
2026-01-14 11:01:10 +0400

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,31 @@
+export default {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat", // New feature
+				"fix", // Bug fix
+				"docs", // Documentation changes
+				"style", // Formatting, missing semi-colons, etc.
+				"refactor", // Refactoring code
+				"perf", // Performance improvements
+				"test", // Adding missing tests
+				"build", // Build system changes
+				"ci", // CI/CD changes
+				"revert", // Revert changes
+				"wip", // Work in progress
+				"chore", // Miscellaneous/other changes
+			],
+		],
+	},
+	"type-case": [2, "always", "lower-case"],
+	"type-empty": [2, "never"],
+	"scope-case": [2, "always", "lower-case"],
+	"subject-empty": [2, "never"],
+	"subject-full-stop": [2, "never", "."],
+	"header-max-length": [2, "always", 100],
+	"body-leading-blank": [1, "always"],
+	"footer-leading-blank": [1, "always"],
+};
